6eb3f657038af2b3023e0fe0ed4a9343d88acd5e
Lalitha A R <lalithaar.research@gmail.com>
2026-06-11 22:05:58 +0530

    fix: move svelte compiler options to config to enable adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
 
 export default {
+	compilerOptions: {
+		runes: ({ filename }) =>
+			filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+	},
 	kit: {
 		adapter: adapter({ fallback: '404.html' }),
 		paths: {
